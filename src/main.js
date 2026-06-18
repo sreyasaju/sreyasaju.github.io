0cb7c1d17c172b403342a1b89b6af075401515d4
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2026-06-19 01:00:28 +0300

    feat: enhance text interaction with mouse movement and update heading style

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 import './style.css'
+
+
+const textElement = document.getElementById('gradient-text');
+
+if (textElement) {
+    window.addEventListener("mousemove", (e) => {
+        // get the bounding box of the text
+        const rect = textElement.getBoundingClientRect();
+
+        // calculate the mouse's position relative to the text element size
+        const x = e.clientX - rect.left;
+        const y = e.clientY -  rect.top;
+
+        const xPercent = (x/ rect.width) * 100 // convert to %
+        const yPercent = (y/ rect.width) * 100
+
+        textElement.style.setProperty("--x", `${xPercent}%`);
+        textElement.style.setProperty("--y", `${yPercent}%`);
+
+    });
+}
